66718a7834388c01624397e3c8561970a5a537f1
Eric Fang <thericfang@Erics-MacBook-Pro-2.local>
2018-11-25 14:19:31 -0500

    working

--- Resource.java
+++
@@ -1,8 +0,0 @@
-public class Resource { 
-    private int resourceNumber;
-    private int units;
-    public Resource (int resourceNumber, int units) {
-        this.resourceNumber = resourceNumber;
-        this.units = units;
-    }   
-}
